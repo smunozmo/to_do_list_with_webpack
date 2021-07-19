1616a269b5f4381963c994a8328a62db307d3ebc
Sebastián M <smunozmo@gmail.com>
2021-07-19 11:24:05 -0500

    Initial deployment commit.

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,7 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+    publicPath: "/to_do_list_with_webpack/",
   },
   module: {
     rules: [
