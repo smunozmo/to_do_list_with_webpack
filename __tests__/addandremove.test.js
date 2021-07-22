7c7b049f461865fe82b171184f102f695bdfbff1
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-07-21 18:14:41 -0600

    add Eslint to project

--- __tests__/addandremove.test.js
+++ __tests__/addandremove.test.js
@@ -2,22 +2,20 @@
  * @jest-environment jsdom
  */
 
-jest.mock('../src/liststructure.js');
 import { PopulateList } from '../src/liststructure.js';
-import { AddList, RemoveTask } from "../src/addandremove.js";
+import { AddList, RemoveTask } from '../src/addandremove.js';
+
+jest.mock('../src/liststructure.js');
 
 describe('addandremove.js', () => {
   describe('Addlist()', () => {
     test('AddList adds an object to the array of tasks', () => {
-      let array = [];
-      document.body.innerHTML =
-      `<div>
+      const array = [];
+      document.body.innerHTML = `<div>
         <input type="text" id="input" value="Task 1">
       </div>`;
 
-      PopulateList.mockImplementation(() => {
-        return true;
-      });
+      PopulateList.mockImplementation(() => true);
 
       const { location } = window;
       delete window.location;
@@ -28,6 +26,55 @@ describe('addandremove.js', () => {
       expect(array).toHaveLength(1);
     });
 
+    test('AddList adds a task with the correct description to the array', () => {
+      const array = [];
+      document.body.innerHTML = `<div>
+        <input type="text" id="input" value="Task 1">
+      </div>`;
 
-  })
-})
+      PopulateList.mockImplementation(() => true);
+
+      const { location } = window;
+      delete window.location;
+      window.location = { reload: jest.fn() };
+
+      AddList(array);
+
+      expect(array[0].description).toBe('Task 1');
+    });
+
+    test('AddList adds a task with the correct description to the array', () => {
+      const array = [];
+      document.body.innerHTML = `<div>
+        <input type="text" id="input" value="Task 1">
+      </div>`;
+
+      PopulateList.mockImplementation(() => true);
+
+      const { location } = window;
+      delete window.location;
+      window.location = { reload: jest.fn() };
+
+      AddList(array);
+
+      expect(array[0].description).not.toBe('Task 2');
+    });
+
+    test('AddList adds a task with an empty value when the input has no value', () => {
+      const array = [];
+      document.body.innerHTML = `<div>
+        <input type="text" id="input" value="">
+      </div>`;
+
+      PopulateList.mockImplementation(() => true);
+
+      const { location } = window;
+      delete window.location;
+      window.location = { reload: jest.fn() };
+
+      AddList(array);
+
+      expect(array[0].description).toBe('');
+    });
+  });
+});
